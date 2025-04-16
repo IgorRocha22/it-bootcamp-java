6da2b3656de7071df205d2e806102aa98a3c2f98
IgorRocha22 <igor.nathan.334@gmail.com>
2025-04-16 12:11:52 -0300

    Remove link tracker da pasta inicial

--- link_tracker/src/main/java/br/com/link/tracker/dto/ExceptionDto.java
+++
@@ -1,5 +0,0 @@
-package br.com.link.tracker.dto;
-
-public record ExceptionDto(String message) {
-
-}
--- link_tracker/src/main/java/br/com/link/tracker/dto/LinkDto.java
+++
@@ -1,5 +0,0 @@
-package br.com.link.tracker.dto;
-
-public record LinkDto(Integer id, String location, Integer countRedirect, boolean valid, String password) {
-
-}
